961c4c3bb3f47072cfaa3e60b10121eb1686b8fe
Carlos Fabian <carl.fab.g@gmail.com>
2020-10-02 09:19:36 -0500

    handleReturn method in HeroScreen component

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById'
 
-const HeroScreen = () => {
+const HeroScreen = ({ history }) => {
 
     const { heroId } = useParams()
     const hero = getHeroById(heroId)
@@ -11,9 +11,32 @@ const HeroScreen = () => {
         return <Redirect to="/" />
     }
 
+    const handleReturn = () => {
+        history.goBack()
+    }
+
     return (
-        <div>
-            <h1>Hero Screen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={`../assets/heroes/${hero.id}.jpg`}
+                    alt={hero.superhero} 
+                    className="img-thumbnail"/>
+            </div>
+
+            <div className="col-8">
+                <h3>{hero.superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b>Alter ego:</b>{ hero.alter_ego }</li>
+                    <li className="list-group-item"> <b>Publisher:</b>{ hero.publisher }</li>
+                    <li className="list-group-item"> <b>First appearence: </b>{ hero.first_appearance }</li>
+                </ul>
+                <h5> Characters </h5>
+                <p>{ hero.characters }</p>
+                <button className="btn btn-outline-info" onClick={handleReturn}>
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
